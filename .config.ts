dbd565c3745ef45b8c94c8c32869853d07dd0486
Jack McCown <jackmac79@gmail.com>
2022-07-07 00:18:44 -0400

    rename

--- .config.ts
+++ .config.ts
@@ -4,8 +4,8 @@ import type { DenonConfig } from "https://deno.land/x/denon/mod.ts";
 const config: DenonConfig = {
   scripts: {
     start: {
-      cmd: "deno run app.ts",
-      desc: "run my app.ts file",
+      cmd: "deno run mod.ts",
+      desc: "run my mod.ts file",
       lock: "lock.json", // check specified lockfile
       unstable: true,
       env: env(),
