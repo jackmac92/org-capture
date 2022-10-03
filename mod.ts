7f8a2375279c3dd59cbe7529562f3738e5d88c39
Jack McCown <jackmac79@gmail.com>
2022-10-02 22:22:04 -0400

    feat: remove concept of title, just use body
    BREAKING CHANGE: pretty much the whole api is a breaking change

--- mod.ts
+++ mod.ts
@@ -7,74 +7,67 @@ export function orgCaptureUrlFactoryCore(captureType: string, options = {}) {
 export function orgCaptureUrlFactoryFactory(
   captureType: string,
   template: string,
-  title: string,
   body: string,
   options = {}
 ) {
   return orgCaptureUrlFactoryCore(captureType, {
     template,
     body,
-    title,
     ...options,
   });
 }
 
-export function orgCaptureHelper(
+export function orgCaptureUrlFactoryFactory(
+  captureType: string,
   template: string,
-  title: string,
   body: string,
   options = {}
 ) {
-  return orgCaptureUrlFactoryFactory(
-    "story-link",
+  return orgCaptureUrlFactoryCore(captureType, {
     template,
-    title,
     body,
-    options
-  );
+    ...options,
+  });
+}
+
+export function orgCaptureHelper(template: string, body: string, options = {}) {
+  return orgCaptureUrlFactoryFactory("story-link", template, body, options);
 }
 
 export function orgCaptureNonInteractive(
   template: string,
-  title: string,
   body: string,
   options = {}
 ) {
-  return orgCaptureUrlFactoryFactory("capture", template, title, body, options);
+  return orgCaptureUrlFactoryFactory("capture", template, body, options);
 }
 
 export function orgCaptureReadable(
   template: string,
-  title: string,
   body: string,
   options = {}
 ) {
   return orgCaptureUrlFactoryFactory(
     "capture-eww-readble",
     template,
-    title,
     body,
     options
   );
 }
 
-export function orgCaptureTodo(title: string, body: string) {
-  orgCaptureNonInteractive("1", title, body);
+export function orgCaptureTodo(body: string) {
+  orgCaptureNonInteractive("1", body);
 }
-export function orgCaptureTodoSync(title: string, body: string) {
-  orgCaptureHelper("2", title, body);
-}
-
-export function orgCaptureNote(title: string, body: string) {
-  orgCaptureNonInteractive("n", title, body);
+export function orgCaptureTodoSync(body: string) {
+  orgCaptureHelper("2", body);
 }
 
-export function orgCaptureDetailsInEmacs(
-  title: string,
-  body: string,
-  options = {}
-) {
-  orgCaptureHelper("fleshout", title, body, options);
+export function orgCaptureNote(body: string) {
+  orgCaptureNonInteractive("n", body);
+}
+
+export function orgCaptureDetailsInEmacs(body: string, options = {}) {
+  orgCaptureHelper("fleshout", body, options);
 }
 
 export function mkOrgLink(link: string, name?: string) {
