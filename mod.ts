4cd4a74faf4c4a8d5766d75b79f406b611b8700b
Jack McCown <jackmac79@gmail.com>
2022-12-22 19:29:07 -0500

    fix: revert previious

--- mod.ts
+++ mod.ts
@@ -72,7 +72,7 @@ export function mkOrgLink(link: string, name?: string) {
   return `[[${link}[[${link}][]]]]`;
 }
 
-export function strictMkOrgLink(link: string) {
+export function strictMkOrgLink(link: string, name: string) {
   if (isOrgLink(link)) {
     return link;
   }
